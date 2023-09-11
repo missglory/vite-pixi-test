dc8d916671ab795415bd2c624f10e17805fb26e9
mg <mg@pop-os.localdomain>
2023-09-11 04:22:22 +0300

    feat: Broken offset

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,25 @@ document.body.appendChild(app.view);
 
 class Ruler extends PIXI.Container {
 
-  constructor(parent, onDragStart, onDragEnd, onDragMove, topOffset = 50, botOffset = 50) {
+  localOffset = new PIXI.Point(0, 0);
+  constructor(
+    parent,
+    onDragStart,
+    onDragEnd,
+    onDragMove,
+    startOffset = 50,
+    endOffset = 50,
+    offset: PIXI.Point = new PIXI.Point(0, 0),
+    initOffset: PIXI.Point = new PIXI.Point(0, 0),
+  ) {
     super();
     parent.stage.addChild(this);
-    this.y = topOffset;
-    let h = 0;
-    const fontSize = 13;
-    const height = window.innerHeight - topOffset - botOffset;
+    const savedOffset = offset.clone();
+    offset = initOffset.clone();
+    this.localOffset = initOffset;
+    this.y = startOffset;
+    const fontSize = 26;
+    const height = window.innerHeight - startOffset - endOffset;
     for (let i = 0; i < height / fontSize; i++) {
       const child = new PIXI.Text((182.01 + i / 100).toFixed(3), new PIXI.TextStyle({ fontFamily: 'JetBrains Mono', fontSize: fontSize }));
       // PIXI.Assets.load('JetBrainsMonoNerdFont-Medium.ttf').then(() => {
@@ -25,15 +37,17 @@ class Ruler extends PIXI.Container {
       //   // color: '#ffffff'
       // },
       // );
-      child.y = h;
-      child.x = 50;
+      child.x = offset.x;
+      child.y = offset.y;
+      offset.x += savedOffset.x;
+      offset.y += savedOffset.y;
       child.cursor = 'pointer';
       child.eventMode = 'static';
       child.on('pointerdown', onDragStart, child);
       child.on('pointerup', onDragEnd);
       // child.on('pointeroutside', onDragEnd);
+      child.scale.set(0.5);
       this.addChild(child);
-      h += 50;
     }
   }
   // }
@@ -58,10 +72,10 @@ window.WebFontConfig = {
   },
 };
 
-(function() {
+(function () {
   const wf = document.createElement('script');
   wf.src = `${document.location.protocol === 'https:' ? 'https' : 'http'
-  }://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
+    }://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
   wf.type = 'text/javascript';
   wf.async = 'true';
   const s = document.getElementsByTagName('script')[0];
@@ -71,45 +85,71 @@ window.WebFontConfig = {
 let lastPos = new PIXI.Point();
 
 function init() {
-    let dragTarget: PIXI.Sprite[] = [];
-    app.stage.eventMode = 'static';
-    app.stage.hitArea = app.screen;
-    app.stage.on('pointerup', onDragEnd);
-    app.stage.on('pointerupoutside', onDragEnd);
+  let dragTarget: PIXI.Sprite[] = [];
+  app.stage.eventMode = 'static';
+  app.stage.hitArea = app.screen;
+  app.stage.on('pointerup', onDragEnd);
+  app.stage.on('pointerupoutside', onDragEnd);
 
-    function onDragMove(event) {
-      // if (dragTarget.length > 0) {
-        // dragTarget.parent.toLocal(event.global, null, dragTarget.position);
-        dragTarget.forEach(d => {
-          if (d.parent instanceof PIXI.Container) {
-            d.parent.toLocal(new PIXI.Point(0, event.global.y - lastPos.y), undefined, d.position);
-          }
-        });
-      // }
-    }
-
-    function onDragStart(event) {
-      // store a reference to the data
-      // the reason for this is because of multitouch
-      // we want to track the movement of this particular touch
-      // this.data = event.data;
-
-      this.alpha = 0.5;
-      dragTarget = [this.parent];
-      app.stage.on('pointermove', onDragMove);
-      lastPos = new PIXI.Point(event.global.x, event.global.y);
-    }
-
-    function onDragEnd() {
-      if (dragTarget) {
-        app.stage.off('pointermove', onDragMove);
-        drag = false;
-        dragTarget.forEach(d => { d.alpha = 1; });
-        dragTarget = [];
+  function onDragMoveV(event) {
+    // if (dragTarget.length > 0) {
+    // dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+    dragTarget.forEach(d => {
+      if (d.parent instanceof PIXI.Container) {
+        d.parent.toLocal(new PIXI.Point(0, event.global.y - lastPos.y), undefined, d.position);
       }
-      lastPos = new PIXI.Point();
+    });
+    // }
+  }
+
+  function onDragMoveH(event) {
+    // if (dragTarget.length > 0) {
+    // dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+    dragTarget.forEach(d => {
+      if (d.parent instanceof PIXI.Container) {
+        d.parent.toLocal(new PIXI.Point(event.global.x - lastPos.x, 0), undefined, d.position);
+      }
+    });
+    // }
+  }
+
+  function onDragStartV(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    // this.data = event.data;
+
+    this.alpha = 0.5;
+    dragTarget = [this.parent];
+    app.stage.on('pointermove', onDragMoveV);
+    lastPos = new PIXI.Point(event.global.x, event.global.y);
+  }
+
+  function onDragStartH(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    // this.data = event.data;
+
+    this.alpha = 0.5;
+    dragTarget = [this.parent];
+    app.stage.on('pointermove', onDragMoveH);
+    lastPos = new PIXI.Point(event.global.x, event.global.y);
+  }
+
+  function onDragEnd() {
+    this.alpha = 1;
+    if (dragTarget) {
+      app.stage.off('pointermove', onDragMoveV);
+      dragTarget.forEach(d => { d.alpha = 1; });
+      dragTarget = [];
     }
-    const ruler = new Ruler(app, onDragStart, onDragEnd, onDragMove,  50, 50);
+    lastPos = new PIXI.Point();
+  }
+  const ruler = new Ruler(app, onDragStartV, onDragEnd, onDragMoveV, 50, 50,
+    new PIXI.Point(0, 13), new PIXI.Point(0, 0));
+  const rulerH = new Ruler(app, onDragStartH, onDragEnd, onDragMoveH, 50, 50,
+    new PIXI.Point(75, 0), new PIXI.Point(75, 0));
 }
 
 
